e24d18c9c236a0a1d086e0c474f19bfaace4708d
julesrx <25800503+julesrx@users.noreply.github.com>
2021-02-19 23:06:43 +0100

    chart height

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -1,9 +1,12 @@
 <template>
-  <li>{{ date }} : {{ acts }}</li>
+  <li>
+    <div :style="{ width: `${pourcent}%` }"></div>
+    {{ date }} : {{ spent }} => ({{ pourcent }}%)
+  </li>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, defineProps } from "vue";
+import { computed, defineComponent } from "vue";
 
 import { CustomHistoricalStatsPeriodGroup } from "../interfaces";
 import activities from "../activities";
@@ -16,11 +19,11 @@ export default defineComponent({
     const intl = Intl.DateTimeFormat();
     const date = computed(() => intl.format((props.day as unknown) as Date));
 
-    const acts = computed(() => {
+    const spent = computed(() => {
       const day = (props.day as unknown) as Date;
       const r = activities.state.dates[day.getFullYear()]?.[day.getMonth() + 1]?.[day.getDate()];
 
-      const seconds = !r
+      return !r
         ? 0
         : r.reduce(
             (a: number, b: CustomHistoricalStatsPeriodGroup) =>
@@ -28,13 +31,24 @@ export default defineComponent({
             0
           );
 
-      const hours = Math.trunc(seconds / 60 / 60);
-      const minutes = Math.trunc(seconds / 60);
+      // const hours = Math.trunc(seconds / 60 / 60);
+      // const minutes = Math.trunc(seconds / 60);
 
-      return `${hours}h ${minutes - hours * 60}m ${seconds - minutes * 60}s (${seconds}s)`;
+      // return `${hours}h ${minutes - hours * 60}m ${seconds - minutes * 60}s (${seconds}s)`;
     });
 
-    return { date, acts };
+    const pourcent = computed(() =>
+      spent.value ? (spent.value / activities.maxTime.value) * 100 : 0
+    );
+
+    return { date, spent, pourcent };
   },
 });
 </script>
+
+<style scoped>
+div {
+  background-color: red;
+  height: 10px;
+}
+</style>
