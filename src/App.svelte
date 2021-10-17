158264ce505b1775d1fd3bf31a7f751ff125eb0e
julesrx <25800503+julesrx@users.noreply.github.com>
2021-10-17 22:56:24 +0200

    store

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
+  import type { UserSearchResponseDetail } from 'bungie-api-ts/user';
+
+  import { profile } from './stores';
   import Search from 'lib/Search.svelte';
   import Calendar from 'lib/Calendar/Calendar.svelte';
+
+  let profile_value: UserSearchResponseDetail;
+  const unsubscribe = profile.subscribe(value => {
+    profile_value = value;
+  });
 </script>
 
 <main class="pl-2 pt-2">
   <!-- What font h1 Pop OS -->
   <h1 class="text-4xl mb-4">Destiny 2 Heatmap</h1>
+
+  {JSON.stringify(profile_value)}
 
   <Search />
   <Calendar />
--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -3,6 +3,8 @@
   import type { ServerResponse } from 'bungie-api-ts/core';
   import type { UserSearchResponse, UserSearchResponseDetail } from 'bungie-api-ts/user';
 
+  import { profile } from '~/stores';
+
   let gamertag: string;
 
   let promise: Promise<UserSearchResponseDetail[]>;
@@ -20,17 +22,16 @@
 </script>
 
 <form class="mb-6">
-  <input type="text" name="Gamertag" placeholder="Gamertag..." bind:value={gamertag} />
+  <input type="text" name="Gamertag" placeholder="Gamertag..." bind:value={gamertag} autofocus />
 
   {#if promise}
     <div>
       {#await promise}
         <p>...searching</p>
       {:then players}
-        <pre>{JSON.stringify(players)}</pre>
         <ul>
           {#each players as player}
-            <li>
+            <li class="flex space-x-2 items-center cursor-pointer" on:click={() => profile.set(player)}>
               <img
                 src={`https://bungie.net${player.destinyMemberships[0].iconPath}`}
                 class="h-8 w-8"
