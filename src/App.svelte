90f0b0dbfc1aa1418e9dfc8bfc44b04377780d48
julesrx <25800503+julesrx@users.noreply.github.com>
2021-10-18 22:06:15 +0200

    note

--- src/App.svelte
+++ src/App.svelte
@@ -39,6 +39,7 @@
     await Promise.all(characters.map(c => fetchActivities(c, 0)));
   });
 
+  // inifnite loop ?
   const fetchActivities = async (character: DestinyCharacterComponent, page: number = 0) => {
     const count = 250;
 
@@ -53,7 +54,9 @@
 
     // updateActivities(res.data.Response.activities);
     activities.update(a => a.concat(res.data.Response.activities));
-    await fetchActivities(character, page + 1);
+
+    console.log(character.characterId, page);
+    fetchActivities(character, page + 1);
   };
 </script>
 
@@ -61,8 +64,23 @@
   <!-- What font h1 Pop OS -->
   <h1 class="text-4xl mb-4">Destiny 2 Heatmap</h1>
 
-  {JSON.stringify(profile_value)}
-
   <Search />
   <Calendar />
 </main>
+
+<p class="text-dark-50 absolute bottom-0 right-0 text-right pb-1 pr-2">
+  Note: this is a blatant copy of <a
+    href="https://chrisfried.github.io/secret-scrublandeux/"
+    target="_blank"
+    rel="noreferrer noopener">Destiny 2 Historical Heatmap</a
+  >
+  by
+  <a target="_blank" rel="noreferrer noopener" href="https://twitter.com/chrisfried">@chrisfried</a
+  >, but done with Svelte, with dark mode and no authentification.
+</p>
+
+<style lang="postcss">
+  a {
+    @apply text-light-blue-900;
+  }
+</style>
