63605b2f2ac921a4311afa231371a08fe614bf3a
julesrx <25800503+julesrx@users.noreply.github.com>
2021-10-21 21:59:21 +0200

    fixed note

--- src/lib/Note.svelte
+++ src/lib/Note.svelte
@@ -1,4 +1,4 @@
-<p class="text-dark-50 absolute bottom-0 right-0 text-right pb-1 pr-2">
+<p class="text-dark-50 fixed bottom-0 right-0 text-right pb-1 pr-2">
   Note: this is a blatant copy of <a
     href="https://chrisfried.github.io/secret-scrublandeux/"
     target="_blank"
@@ -6,7 +6,7 @@
   >
   by
   <a target="_blank" rel="noreferrer noopener" href="https://twitter.com/chrisfried">@chrisfried</a
-  >, but done with Svelte, with dark mode and no authentification.
+  >,<br /> but done with Svelte, with dark mode and no authentification.
 </p>
 
 <style lang="postcss">
